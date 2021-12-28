60451ec50fe7d0e2108df0654741e9461de22f41
Rane Brown <rane.brown@gmail.com>
2021-12-28 13:41:07 -0800

    Add lightspeed

--- lua/kobol/plugins_theme.lua
+++ lua/kobol/plugins_theme.lua
@@ -78,64 +78,19 @@ local plugins = lush(function()
         NotifyDEBUGBody { base.Normal },
         NotifyTRACEBody { base.Normal },
 
-        -- lightspeed
-        LightspeedLabel {},
-        -- The character needed to be pressed to jump to the match position,
-        -- after the whole search pattern has been given. It appears once the
-        -- first input has been entered, right after the pair (by default), or on
-        -- top of the second character (when the beacon should be squeezed into
-        -- the 2-column box of the match).
-
-        -- LightspeedLabelOverlapped {},
-        -- When matches overlap, labels get next to each other - in that case
-        -- they get different highlights, to be more easily distinguishable
-        -- (slightly lighter or darker, depending on the global background).
-
-        -- LightspeedLabelDistant {},
-        -- LightspeedLabelDistantOverlapped {},
-        -- If the number of matches exceeds the available target labels, the next
-        -- group of labeled targets are shown with a different color. Those can
-        -- be reached by pressing `cycle_group_fwd_key` before the label
-        -- character.
-
-        -- LightspeedShortcut {},
-        -- LightspeedShortcutOverlapped {},
-        -- Labels for positions that can be jumped to right after the first
-        -- input (see |lightspeed-shortcuts|).
-        -- These are highlighted as "inverted" labels by default
-        -- (background/foreground switched).
-
-        -- LightspeedMaskedChar {},
-        -- The second character of the match, that is shown on top of the first
-        -- one, as a reminder, when the beacon is squeezed into the 2-column box
-        -- of the match.
-
-        -- LightspeedGreyWash {},
-        -- Foreground color of the "washed out" area for 2-character search.
-        -- Depending on the colorscheme, it might be appropriate to link this to
-        -- the Comment highlight group.
-
-        -- LightspeedUnlabeledMatch {},
-        -- Matches that can be jumped to automatically, i.e. do not get a label
-        -- - the only ones, and the first ones if `jump_to_first_match` is on.
-        -- (Bold black or white by default, depending on the global background.)
-
-        -- LightspeedOneCharMatch {},
-        -- Matching characters of f/t search. (Default: |LightspeedShortcut|
-        -- without underline. Setting some background color is recommended, as
-        -- there is no "grey wash" for one-character search mode.)
-
-        -- LightspeedUniqueChar {},
-        -- Unique characters in the search direction, shown if
-        -- `highlight_unique_chars` is on. Uses the same settings as
-        -- |LightspeedUnlabeledMatch| by default.
-
-        -- LightspeedPendingOpArea {},
-        -- When jumping based on partial input in operator-pending mode, we do
-        -- not see the operation executed right away, because of the "safety"
-        -- timeout (see |lightspeed-jump-on-partial-input|), therefore we set a
-        -- temporary highlight on the operated area.
-
+        -- Lightspeed
+        LightspeedLabel { fg = c.yellow, gui = "bold,underline" },
+        LightspeedLabelOverlapped { fg = c.yellow, gui = "underline" },
+        LightspeedLabelDistant { fg = c.blue, gui = "bold,underline" },
+        LightspeedLabelDistantOverlapped { fg = c.blue, gui = "underline" },
+        LightspeedShortcut { fg = c.yellow, gui = "bold,underline,reverse" },
+        LightspeedShortcutOverlapped { fg = c.yellow, gui = "underline,reverse" },
+        LightspeedMaskedChar { fg = c.fg4 },
+        LightspeedGreyWash { base.Comment },
+        LightspeedUnlabeledMatch { fg = c.fg, gui = "bold" },
+        LightspeedOneCharMatch { fg = c.yellow, gui = "bold,reverse" },
+        LightspeedUniqueChar {LightspeedUnlabeledMatch},
+        LightspeedPendingOpArea {base.Visual},
         LightspeedCursor { base.Cursor },
     }
 end)
